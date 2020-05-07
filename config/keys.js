f1be8847db6f1be895219b0e5656bd10705869f8
acwcheung <acwcheung1@gmail.com>
2020-05-07 12:03:51 +0800

    fixed keys file

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 //test the node environment
 //import the right api
 
-if(process.env.NOD_ENV === 'production') {
+if(process.env.NODE_ENV === 'production') {
 	module.exports = require('./keys_prod');
 } else {
 	module.exports = require('./keys_dev');
